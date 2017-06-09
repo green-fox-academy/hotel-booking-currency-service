795eeed7c14717c4a202c778c49067ce49d546d9
Zsombor Balogh <balogh.zsombor.92@gmail.com>
2017-06-09 09:59:48 +0200

    recorrect the test

--- src/test/java/com/hiddenite/controller/CurrencyRestControllerTest.java
+++ src/test/java/com/hiddenite/controller/CurrencyRestControllerTest.java
@@ -75,7 +75,7 @@ public class CurrencyRestControllerTest {
             .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(content().contentType(contentType))
-            .andExpect(content().json("{\"database\": \"okassa\"}"));
+            .andExpect(content().json("{\"database\": \"ok\"}"));
   }
 
   @Test
