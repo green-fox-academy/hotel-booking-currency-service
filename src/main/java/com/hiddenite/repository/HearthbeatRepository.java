e80075d3059ab5d9a6ec3681920e18a29c3f1c14
Kerdo Andrea <kerdoandi@gmail.com>
2017-06-09 16:33:56 +0200

    gradlew a+x

--- src/main/java/com/hiddenite/repository/HearthbeatRepository.java
+++ src/main/java/com/hiddenite/repository/HearthbeatRepository.java
@@ -1,9 +1,12 @@
 package com.hiddenite.repository;
 
 import com.hiddenite.model.Hearthbeat;
-import java.util.ArrayList;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
+@Repository
 public interface HearthbeatRepository extends CrudRepository<Hearthbeat, Long> {
 
   public ArrayList<Hearthbeat> findAll();
