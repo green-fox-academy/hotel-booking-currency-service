a850b1fb55d8b51c6eb759b8ee80ef45a6fd15b3
Zsombor Balogh <balogh.zsombor.92@gmail.com>
2017-06-13 17:45:52 +0200

    modify properties

--- src/main/java/com/hiddenite/controller/CurrencyRestController.java
+++ src/main/java/com/hiddenite/controller/CurrencyRestController.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
 
+
 @RestController
 public class CurrencyRestController {
 
   private HeartbeatRepository heartbeatRepository;
   static Logger log = Logger.getLogger(MethodHandles.lookup().lookupClass());
+
 
 
   @Autowired
