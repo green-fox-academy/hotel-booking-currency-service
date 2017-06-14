01911d10c8a9ce0871624c46ad2a2b28b852506c
Zsombor Balogh <balogh.zsombor.92@gmail.com>
2017-06-14 09:31:37 +0200

    add h2 dependency

--- src/test/java/com/hiddenite/controller/CurrencyRestControllerTest.java
+++ src/test/java/com/hiddenite/controller/CurrencyRestControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
