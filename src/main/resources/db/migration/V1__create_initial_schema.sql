4a2cdfb5e25163b681416581a5f9b377286e4536
Zsombor Balogh <balogh.zsombor.92@gmail.com>
2017-06-14 09:37:39 +0200

    remove schema

--- src/main/resources/db/migration/V1__create_initial_schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE `hearthbeat` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-CREATE TABLE `hearthbeat` (
-  `status` bit(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
