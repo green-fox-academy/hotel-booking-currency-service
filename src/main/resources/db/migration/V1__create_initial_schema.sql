d845463ffb2c726102a47ce78eb7602eac7c44a5
Zsombor Balogh <balogh.zsombor.92@gmail.com>
2017-06-14 09:35:03 +0200

    remove query from schema

--- src/main/resources/db/migration/V1__create_initial_schema.sql
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE `hearthbeat` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 CREATE TABLE `hearthbeat` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `status` bit(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
