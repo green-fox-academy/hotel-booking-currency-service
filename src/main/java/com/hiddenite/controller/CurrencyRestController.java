91bf67b7723c050c5fdac3beb99479976ad09778
Zsombor Balogh <balogh.zsombor.92@gmail.com>
2017-06-15 13:40:32 +0200

    set host

--- src/main/java/com/hiddenite/controller/CurrencyRestController.java
+++ src/main/java/com/hiddenite/controller/CurrencyRestController.java
@@ -3,12 +3,13 @@ package com.hiddenite.controller;
 import com.hiddenite.model.Status;
 //import com.hiddenite.service.MQService;
 import com.hiddenite.service.StatusService;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.concurrent.TimeoutException;
@@ -26,14 +27,14 @@ public class CurrencyRestController {
   StatusService statusService;
 
   @GetMapping("/heartbeat")
-  public Status getStatus(HttpServletRequest request) throws IOException, TimeoutException {
+  public Status getStatus(javax.servlet.http.HttpServletRequest request) throws IOException, TimeoutException {
 //    mqService.sendMessageToQueue("heartbeat", "Hello World!");
     Status status = statusService
             .checkStatusCondition(1, true);
     if (status.everythingIsOk()) {
       log.info("HTTP-REQUEST " + request.getRequestURI());
     } else {
-      log.error("HTTP-ERROR" + request.getRequestURI());
+      log.error("HTTP-ERROR " + request.getRequestURI());
     }
     return status;
   }
--- src/main/java/com/hiddenite/service/MQService.java
+++ src/main/java/com/hiddenite/service/MQService.java
@@ -1,57 +1,57 @@
-//package com.hiddenite.service;
-//
-//import java.io.IOException;
-//import java.net.URI;
-//import java.net.URISyntaxException;
-//import java.security.KeyManagementException;
-//import java.security.NoSuchAlgorithmException;
-//import java.util.concurrent.TimeoutException;
-//
-//import com.rabbitmq.client.AMQP;
-//import org.springframework.stereotype.Service;
-//import com.rabbitmq.client.ConnectionFactory;
-//import com.rabbitmq.client.Connection;
-//import com.rabbitmq.client.Channel;
-//
-//@Service
-//public class MQService {
-//
-//  Channel channel;
-//  Connection connection;
-//
-//  public MQService() {
-//  }
-//
-//  public void sendMessageToQueue(String queue, String message)
-//          throws IOException, TimeoutException {
-//    ConnectionFactory factory = new ConnectionFactory();
-////    factory.setUsername("");
-////    factory.setPassword("");
-//    factory.setHost("localhost");
-////    factory.setUri(new URI(""));
-//
-//
-//
-//
-//
-//
-//    connection = factory.newConnection();
-//    channel = connection.createChannel();
-//    channel.queueDeclare(queue, false, false, true, null);
-//    channel.basicPublish("", queue, null, message.getBytes("UTF-8"));
-//    System.out.println(" [x] Sent '" + message + "'");
-////    channel.close();
-////    connection.close();
-//  }
-//
-//  public int getQueueMessageCount(String queue) throws IOException {
-//    AMQP.Queue.DeclareOk ok = channel.queueDeclarePassive(queue);
-//    return ok.getMessageCount();
-//  }
-//
-//  public boolean isConnected() {
-//    return channel.isOpen();
-//  }
-//
-//}
-//
+package com.hiddenite.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP;
+import org.springframework.stereotype.Service;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+@Service
+public class MQService {
+
+  Channel channel;
+  Connection connection;
+
+  public MQService() {
+  }
+
+  public void sendMessageToQueue(String queue, String message)
+          throws IOException, TimeoutException {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setUsername("dmyuipta");
+    factory.setPassword("WNz9LJE3Dchk9dtnAZEL5jo9v7pETTwO");
+    factory.setHost("fish.rmq.cloudamqp.com");
+//    factory.setUri(new URI(""));
+
+
+
+
+
+
+    connection = factory.newConnection();
+    channel = connection.createChannel();
+    channel.queueDeclare(queue, false, false, true, null);
+    channel.basicPublish("", queue, null, message.getBytes("UTF-8"));
+    System.out.println(" [x] Sent '" + message + "'");
+//    channel.close();
+//    connection.close();
+  }
+
+  public int getQueueMessageCount(String queue) throws IOException {
+    AMQP.Queue.DeclareOk ok = channel.queueDeclarePassive(queue);
+    return ok.getMessageCount();
+  }
+
+  public boolean isConnected() {
+    return channel.isOpen();
+  }
+
+}
+
