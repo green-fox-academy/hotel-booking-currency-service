1a334b640b1a0305437edb3f946d39a5b3d38810
Zsombor Balogh <balogh.zsombor.92@gmail.com>
2017-06-21 10:46:25 +0200

    add charge endpoint and stripeservice

---
+++ src/main/java/com/hiddenite/controller/ChargeController.java
@@ -0,0 +1,41 @@
+package com.hiddenite.controller;
+
+import com.hiddenite.model.ChargeRequest;
+import com.hiddenite.repository.ChargeRequestRepository;
+import com.hiddenite.service.StripeService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ChargeController {
+
+  @Autowired
+  private StripeService paymentsService;
+  @Autowired
+  private ChargeRequestRepository chargeRequestRepository;
+
+  @PostMapping("/charge")
+  public String charge(ChargeRequest chargeRequest, Model model)
+          throws StripeException {
+    chargeRequest.setDescription("Example charge");
+    chargeRequest.setCurrency(ChargeRequest.Currency.EUR);
+    Charge charge = paymentsService.charge(chargeRequest);
+    model.addAttribute("id", charge.getId());
+    model.addAttribute("status", charge.getStatus());
+    model.addAttribute("chargeId", charge.getId());
+    model.addAttribute("balance_transaction", charge.getBalanceTransaction());
+    chargeRequestRepository.save(chargeRequest);
+    return "result";
+  }
+
+  @ExceptionHandler(StripeException.class)
+  public String handleError(Model model, StripeException ex) {
+    model.addAttribute("error", ex.getMessage());
+    return "result";
+  }
+}
--- src/main/java/com/hiddenite/controller/CheckoutController.java
+++ src/main/java/com/hiddenite/controller/CheckoutController.java
@@ -20,4 +20,12 @@ public class CheckoutController {
     return "checkout";
   }
 
+  @RequestMapping("/charge")
+  public String charge(Model model) {
+    model.addAttribute("amount", 50 * 100);
+    model.addAttribute("stripePublicKey", stripePublicKey);
+    model.addAttribute("currency", ChargeRequest.Currency.EUR);
+    return "checkout";
+  }
+
 }
