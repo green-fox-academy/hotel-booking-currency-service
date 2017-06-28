de8c5de3bc65e89363f08311793aeb2f99e337fc
Kallay Levente <kallaylevente@gmail.com>
2017-06-28 10:46:01 +0200

    fix delete test after pull broke it

--- src/test/java/com/hiddenite/controller/CheckoutsRestControllerTest.java
+++ src/test/java/com/hiddenite/controller/CheckoutsRestControllerTest.java
@@ -84,9 +84,9 @@ public class CheckoutsRestControllerTest {
     checkOutRepository.deleteAll();
     checkOutRepository.save(EURcheckout);
     checkOutRepository.save(USDcheckout);
+    assertEquals(checkOutRepository.count(), 2);
     mockMvc.perform(delete("/api/checkouts/2"))
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.self").value("https://your-hostname.com/api/checkout/2"))
     ;
     assertEquals(checkOutRepository.count(), 1);
 
