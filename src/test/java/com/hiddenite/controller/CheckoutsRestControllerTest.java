71d053f487b0564e6c440bb28127ac492efc6578
Zsombor Balogh <balogh.zsombor.92@gmail.com>
2017-06-28 11:22:38 +0200

    fix 2 tests

--- src/test/java/com/hiddenite/controller/CheckoutsRestControllerTest.java
+++ src/test/java/com/hiddenite/controller/CheckoutsRestControllerTest.java
@@ -1,10 +1,12 @@
 package com.hiddenite.controller;
 
+import com.google.gson.Gson;
 import com.hiddenite.CurrencyApplication;
 import com.hiddenite.model.ChargeRequest;
 import com.hiddenite.model.checkout.Checkout;
 import com.hiddenite.model.checkout.CheckoutAttribute;
 import com.hiddenite.model.checkout.CheckoutData;
+import com.hiddenite.model.error.ErrorMessage;
 import com.hiddenite.repository.CheckOutRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -35,8 +37,8 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 public class CheckoutsRestControllerTest {
 
   private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-      MediaType.APPLICATION_JSON.getSubtype(),
-      Charset.forName("utf8"));
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
   private MockMvc mockMvc;
 
   @Autowired
@@ -53,23 +55,13 @@ public class CheckoutsRestControllerTest {
   public void setup() throws Exception {
     mockMvc = webAppContextSetup(webApplicationContext).build();
     CheckoutAttribute checkoutAttribute = new CheckoutAttribute(1L, 1L, 5000,
-        EUR, "pending");
+            EUR, "pending");
     CheckoutData checkoutData = new CheckoutData("checkout", checkoutAttribute);
     EURcheckout = new Checkout(checkoutData);
     CheckoutAttribute checkoutAttribute2 = new CheckoutAttribute(2L, 2L, 8000,
-        ChargeRequest.Currency.USD, "not so pending");
+            ChargeRequest.Currency.USD, "not so pending");
     CheckoutData checkoutData2 = new CheckoutData("checkout", checkoutAttribute2);
     USDcheckout = new Checkout(checkoutData2);
-    updateCheckout = "   {\n" +
-            "     \"data\": {\n" +
-            "       \"type\": \"checkouts\",\n" +
-            "       \"id\": \"1\",\n" +
-            "       \"attributes\": {\n" +
-            "         \"currency\": \"USD\",\n" +
-            "         \"status\": \"success\"\n" +
-            "       }\n" +
-            "     }\n" +
-            "   }";
   }
 
   @Test
@@ -78,10 +70,10 @@ public class CheckoutsRestControllerTest {
     checkOutRepository.save(EURcheckout);
     checkOutRepository.save(USDcheckout);
     mockMvc.perform(get("/checkouts")
-        .param("currency", "EUR"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.data.length()").value(1))
-        .andExpect(jsonPath("$.data[:1].attributes.currency").value("EUR"));
+            .param("currency", "EUR"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.length()").value(1))
+            .andExpect(jsonPath("$.data[:1].attributes.currency").value("EUR"));
   }
 
   @Test
@@ -90,8 +82,8 @@ public class CheckoutsRestControllerTest {
     checkOutRepository.save(EURcheckout);
     checkOutRepository.save(USDcheckout);
     assertEquals(checkOutRepository.count(), 2);
-    mockMvc.perform(delete("/api/checkouts/2"))
-        .andExpect(status().isOk())
+    mockMvc.perform(delete("/api/checkouts/" + USDcheckout.getId()))
+            .andExpect(status().isOk())
     ;
     assertEquals(checkOutRepository.count(), 1);
 
@@ -100,10 +92,12 @@ public class CheckoutsRestControllerTest {
   @Test
   public void responseToNoIndexGetCheckout() throws Exception {
     checkOutRepository.deleteAll();
-    mockMvc.perform(get("/api/checkouts/888")
-        .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().is4xxClientError())
-        .andExpect(content().contentType(contentType));
+    ErrorMessage errorMessage = new ErrorMessage(404, "Not found", "No checkouts found by id: 666");
+    Gson gson = new Gson();
+    mockMvc.perform(get("/api/checkouts/666"))
+            .andExpect(status().is4xxClientError())
+            .andExpect(content().contentType(contentType))
+            .andExpect(content().json(gson.toJson(errorMessage)));
   }
 
   @Test
@@ -111,9 +105,11 @@ public class CheckoutsRestControllerTest {
     checkOutRepository.deleteAll();
     checkOutRepository.save(EURcheckout);
     checkOutRepository.save(USDcheckout);
-    mockMvc.perform(patch("/api/checkouts/1")
+    USDcheckout.setId(EURcheckout.getId());
+    Gson gson = new Gson();
+    mockMvc.perform(patch("/api/checkouts/" + EURcheckout.getId())
             .contentType(MediaType.APPLICATION_JSON)
-            .content(updateCheckout))
+            .content(gson.toJson(USDcheckout)))
             .andExpect(status().isOk())
             .andExpect(content().contentType(contentType))
             .andExpect(jsonPath("$.data.attributes.booking_id").value("1"))
@@ -122,6 +118,4 @@ public class CheckoutsRestControllerTest {
             .andExpect(jsonPath("$.data.attributes.currency").value("USD"))
             .andExpect(jsonPath("$.data.attributes.status").value("success"));
   }
-
-
 }
