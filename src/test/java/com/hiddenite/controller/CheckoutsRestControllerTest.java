3ed5892de6990b48b73dac22e90c610785f63b1c
Zsombor Balogh <balogh.zsombor.92@gmail.com>
2017-06-28 10:38:28 +0200

    finish no index exception test

--- src/test/java/com/hiddenite/controller/CheckoutsRestControllerTest.java
+++ src/test/java/com/hiddenite/controller/CheckoutsRestControllerTest.java
@@ -1,11 +1,12 @@
 package com.hiddenite.controller;
 
+import com.google.gson.Gson;
 import com.hiddenite.CurrencyApplication;
 import com.hiddenite.model.ChargeRequest;
-import com.hiddenite.model.ChargeRequest.Currency;
 import com.hiddenite.model.checkout.Checkout;
 import com.hiddenite.model.checkout.CheckoutAttribute;
 import com.hiddenite.model.checkout.CheckoutData;
+import com.hiddenite.model.error.ErrorMessage;
 import com.hiddenite.repository.CheckOutRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,20 +19,14 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 import java.nio.charset.Charset;
 
 import static com.hiddenite.model.ChargeRequest.Currency.EUR;
-
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
@@ -41,8 +36,8 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 public class CheckoutsRestControllerTest {
 
   private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-      MediaType.APPLICATION_JSON.getSubtype(),
-      Charset.forName("utf8"));
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
   private MockMvc mockMvc;
 
   @Autowired
@@ -58,11 +53,11 @@ public class CheckoutsRestControllerTest {
   public void setup() throws Exception {
     mockMvc = webAppContextSetup(webApplicationContext).build();
     CheckoutAttribute checkoutAttribute = new CheckoutAttribute(1L, 1L, 5000,
-        EUR, "pending");
+            EUR, "pending");
     CheckoutData checkoutData = new CheckoutData("checkout", checkoutAttribute);
     EURcheckout = new Checkout(checkoutData);
     CheckoutAttribute checkoutAttribute2 = new CheckoutAttribute(2L, 2L, 8000,
-        ChargeRequest.Currency.USD, "not so pending");
+            ChargeRequest.Currency.USD, "not so pending");
     CheckoutData checkoutData2 = new CheckoutData("checkout", checkoutAttribute2);
     USDcheckout = new Checkout(checkoutData2);
   }
@@ -73,10 +68,10 @@ public class CheckoutsRestControllerTest {
     checkOutRepository.save(EURcheckout);
     checkOutRepository.save(USDcheckout);
     mockMvc.perform(get("/checkouts")
-        .param("currency", "EUR"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.data.length()").value(1))
-        .andExpect(jsonPath("$.data[:1].attributes.currency").value("EUR"));
+            .param("currency", "EUR"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.length()").value(1))
+            .andExpect(jsonPath("$.data[:1].attributes.currency").value("EUR"));
   }
 
   @Test
@@ -85,21 +80,21 @@ public class CheckoutsRestControllerTest {
     checkOutRepository.save(EURcheckout);
     checkOutRepository.save(USDcheckout);
     mockMvc.perform(delete("/api/checkouts/2"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.self").value("https://your-hostname.com/api/checkout/2"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.self").value("https://your-hostname.com/api/checkout/2"))
     ;
     assertEquals(checkOutRepository.count(), 1);
-
   }
 
   @Test
   public void responseToNoIndexGetCheckout() throws Exception {
     checkOutRepository.deleteAll();
-    mockMvc.perform(get("/api/checkouts/888")
-        .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().is4xxClientError())
-        .andExpect(content().contentType(contentType));
+    Gson gson = new Gson();
+    ErrorMessage errorMessage = new ErrorMessage(404, "Not found", "No checkouts found by id: 666");
+    mockMvc.perform(get("/api/checkouts/666")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().is4xxClientError())
+            .andExpect(content().contentType(contentType))
+            .andExpect(content().json(gson.toJson(errorMessage)));
   }
-
-
 }
