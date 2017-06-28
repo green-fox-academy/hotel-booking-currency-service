a03b8c3dca01ac34632f30e9bc57478a695d9231
Kallay Levente <kallaylevente@gmail.com>
2017-06-28 09:24:57 +0200

    add filterTest

--- src/test/java/com/hiddenite/controller/CheckoutsRestControllerTest.java
+++ src/test/java/com/hiddenite/controller/CheckoutsRestControllerTest.java
@@ -2,6 +2,7 @@ package com.hiddenite.controller;
 
 import com.hiddenite.CurrencyApplication;
 import com.hiddenite.model.ChargeRequest;
+import com.hiddenite.model.ChargeRequest.Currency;
 import com.hiddenite.model.checkout.Checkout;
 import com.hiddenite.model.checkout.CheckoutAttribute;
 import com.hiddenite.model.checkout.CheckoutData;
@@ -20,7 +21,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.nio.charset.Charset;
 
+import static com.hiddenite.model.ChargeRequest.Currency.EUR;
+
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -29,9 +34,10 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @WebAppConfiguration
 @EnableWebMvc
 public class CheckoutsRestControllerTest {
+
   private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-          MediaType.APPLICATION_JSON.getSubtype(),
-          Charset.forName("utf8"));
+      MediaType.APPLICATION_JSON.getSubtype(),
+      Charset.forName("utf8"));
   private MockMvc mockMvc;
 
   @Autowired
@@ -46,18 +52,27 @@ public class CheckoutsRestControllerTest {
   @Before
   public void setup() throws Exception {
     mockMvc = webAppContextSetup(webApplicationContext).build();
-    CheckoutAttribute checkoutAttribute = new CheckoutAttribute(1L, 1L, 5000, ChargeRequest.Currency.EUR, "pending");
+    CheckoutAttribute checkoutAttribute = new CheckoutAttribute(1L, 1L, 5000,
+        EUR, "pending");
     CheckoutData checkoutData = new CheckoutData("checkout", checkoutAttribute);
     EURcheckout = new Checkout(checkoutData);
+    CheckoutAttribute checkoutAttribute2 = new CheckoutAttribute(2L, 2L, 8000,
+        ChargeRequest.Currency.USD, "not so pending");
+    CheckoutData checkoutData2 = new CheckoutData("checkout", checkoutAttribute2);
+    USDcheckout = new Checkout(checkoutData2);
   }
 
   @Test
   public void testFilterCheckouts() throws Exception {
     checkOutRepository.deleteAll();
     checkOutRepository.save(EURcheckout);
-//    checkOutRepository.save(USDcheckout);
+    checkOutRepository.save(USDcheckout);
     mockMvc.perform(get("/checkouts")
-            .param("currency", "EUR"))
-            .andExpect(status().isOk());
+        .param("currency", "EUR"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.data.length()"). value(1))
+        .andExpect(jsonPath("$.data[:1].attributes.currency").value("EUR"));
+
+
   }
 }
