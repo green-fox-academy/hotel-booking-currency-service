a1854078350e6d280d49544a3a15e5876f86a7cc
Kallay Levente <kallaylevente@gmail.com>
2017-06-29 15:13:15 +0200

    add some feature to transactions

--- src/main/java/com/hiddenite/model/Transaction.java
+++ src/main/java/com/hiddenite/model/Transaction.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import org.springframework.web.bind.annotation.RestController;
 
 @Entity
 public class Transaction {
